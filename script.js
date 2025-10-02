d8bd968caee7b4d1bd0102831edb4303c0ab6a21
Suraj Pal <surajpal7442@gmail.com>
2025-10-02 09:33:11 +0530

    add gsap

--- script.js
+++ script.js
@@ -2,3 +2,22 @@ const scroll = new LocomotiveScroll({
     el: document.querySelector('#main'),
     smooth: true
 });
+
+function firstPageAnim(){
+    var tl = gsap.timeline();
+
+    tl.from("#nav", {
+        y: '-10',
+        opacity: 0,
+        ease: Expo
+    })
+}
+
+function circleMouseFollower(){
+    window.addEventListener("mousemove", function(dets){
+        document.querySelector("#minicircle").style.transform = `translate(${dets.clientX}px, ${dets.clientY}px)`;
+    })
+}
+
+circleMouseFollower();
+firstPageAnim();
