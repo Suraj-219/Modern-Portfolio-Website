f8b9842ee4bacc863435ead1a0024f1e4ec6c1a4
Suraj Pal <surajpal7442@gmail.com>
2025-10-03 18:55:49 +0530

    add  animation

--- script.js
+++ script.js
@@ -9,9 +9,42 @@ function firstPageAnim(){
     tl.from("#nav", {
         y: '-10',
         opacity: 0,
-        ease: Expo
+        duration: 1.5,
+        ease: Expo.easeInOut
+    })
+        .to(".boundingelem", {
+            y: 0,
+            ease: Expo.easeInOut,
+            duration: 2,
+            delay: -1,
+            stagger: .2
+        })
+        .from("#herofooter", {
+            y: -10,
+            opacity: 0,
+            duration: 1.5,
+            delay: -1,
+            ease: Expo.easeInOut
+        })
+}
+
+function circleChaptaKaro(){
+    var xscale = 1;
+    var yscale = 1;
+
+    var xprev = 0;
+    var yprev = 0;
+
+    window.addEventListener("mousemove", (dets)=> {
+        var xdiff = dets.clientX - xprev;
+        var ydiff = dets.clientY - yprev;
+
+        xprev = dets.clientX;
+        yprev = dets.clientY;
     })
 }
+
+circleChaptaKaro();
 
 function circleMouseFollower(){
     window.addEventListener("mousemove", function(dets){
